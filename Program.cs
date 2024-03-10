f56a37a01a25fffa8c2d1284572000588a1fc83e
K Hoyi <komla_h@hotmail.com>
2024-03-10 20:10:42 +0000

    Feature 1 added to Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//NEW THIS 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd2b71599ff81fe7469293b9c17ed1e16066681")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
